a78d59b49e4e0e15d2686a3ad9e10faf80ca2516
daryda9 <daryda9@gmail.com>
2020-02-15 18:49:59 +0100

    fixed histogram RT

--- TWspark/Script/dashboard.py
+++ TWspark/Script/dashboard.py
@@ -94,8 +94,11 @@ class Dashboard:
     def show_histogram(self):
         neg_dict = dict.fromkeys(self.popular_hashtag[:50], 0)
         pos_dict = dict.fromkeys(self.popular_hashtag[:50], 0)
-        x = range(0, len(neg_dict.keys()))
-        x1 = [i + 0.6 for i in x]
+
+        labels = pos_dict.keys()
+        x = np.arange(len(labels))  # posizione dei livelli
+        width = 0.35  # spessore delle barre
+
         with open(self.DATASET_FILE, "r") as raw_data:
             reader = csv.reader(raw_data, delimiter=",")
             for line in reader:
@@ -107,12 +110,80 @@ class Dashboard:
                             neg_dict[tag] += 1
 
         self.ax3.clear()
-        self.ax3.bar(x, pos_dict.values(), width=0.5, color='y', align='center')
-        self.ax3.bar(x1, neg_dict.values(), width=0.5, color='g', align='center')
+
+        fig, ax = plt.subplots()
+        rects1 = self.ax3.bar(x - width / 2, pos_dict.values(), width, label='Positive', color='y')
+        rects2 = self.ax3.bar(x + width / 2, neg_dict.values(), width, label='Negative', color='g')
+
+
+        ax.set_xticks(x)
+
+        def autolabel(rects):
+            """Attach a text label above each bar in *rects*, displaying its height."""
+            for rect in rects:
+                height = rect.get_height()
+                ax.annotate('{}'.format(height),
+                            xy=(rect.get_x() + rect.get_width() / 2, height),
+                            xytext=(0, 3),  # 3 points vertical offset
+                            textcoords="offset points",
+                            ha='center', va='bottom')
+
+        autolabel(rects1)
+        autolabel(rects2)
+
+        fig.tight_layout()
+
+
         self.ax3.set_xticks(np.arange(len(neg_dict.keys())))
         self.ax3.set_xticklabels(neg_dict.keys(), rotation=90)
         # add legenda
         self.ax3.autoscale()
+
+    def show_histogram_2(self):
+        neg_dict = dict.fromkeys(self.popular_hashtag[:50], 0)
+        pos_dict = dict.fromkeys(self.popular_hashtag[:50], 0)
+
+        with open(self.DATASET_FILE, "r") as raw_data:
+            reader = csv.reader(raw_data, delimiter=",")
+            for line in reader:
+                for tag in neg_dict.keys():
+                    if tag in line[0]:
+                        if float(line[1]) > 0.0:  # positive
+                            pos_dict[tag] += 1
+                        if float(line[1]) < 1.0:  # negative
+                            neg_dict[tag] += 1
+
+        labels = pos_dict.keys()
+
+        x = np.arange(len(labels))  # the label locations
+        width = 0.35  # the width of the bars
+
+        fig, ax = plt.subplots()
+        rects1 = ax.bar(x - width / 2, pos_dict.values(), width, label='Positive', color='y')
+        rects2 = ax.bar(x + width / 2, neg_dict.values(), width, label='Negative', color='g')
+
+        # Add some text for labels, title and custom x-axis tick labels, etc.
+        ax.set_ylabel('Sentiment')
+        ax.set_title('Polarità nei giorni')
+        ax.set_xticks(x)
+        ax.set_xticklabels(labels, rotation=40)
+        ax.legend()
+
+        def autolabel(rects):
+            """Attach a text label above each bar in *rects*, displaying its height."""
+            for rect in rects:
+                height = rect.get_height()
+                ax.annotate('{}'.format(height),
+                            xy=(rect.get_x() + rect.get_width() / 2, height),
+                            xytext=(0, 3),  # 3 points vertical offset
+                            textcoords="offset points",
+                            ha='center', va='bottom')
+
+        autolabel(rects1)
+        autolabel(rects2)
+        fig.tight_layout()
+        ax.invert_xaxis()
+        plt.show()
 
 
 def run():
