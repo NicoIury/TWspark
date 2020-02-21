995f1e28b808796108b525d6e4daa1d4ee28de31
daryda9 <daryda9@gmail.com>
2020-02-21 18:09:04 +0100

    small GUI fix

--- TWspark/Script/GUI.py
+++ TWspark/Script/GUI.py
@@ -1,114 +1,98 @@
 from tkinter import *
-from tkinter import scrolledtext
-from tkinter import ttk
+import os
+import threading
 
-font=("Arial Black",10)
+font = ("Arial Black", 10)
+
 
 class GUI:
-    def main(self):
-        self.root_win()
-
-    def root_win(self):
-
-        self.window= Tk()
+    def __init__(self, root):
+        self.window = root
         self.window.title("BD_analisi")
-        self.window.geometry('900x350')
 
-        self.lbl = Label(self.window, text="inserisci 3 parole chiave:", font=font)
+        self.lbl = Label(self.window, text="Enter keywords:", font=font)
         self.lbl.grid(column=0, row=0)
 
-        self.txt = Entry(self.window, width=20)
+        self.txt = Entry(self.window, width=20, bg='white')
         self.txt.grid(column=0, row=1)
-        self.txt.bind("<Return>", lambda event: self.inserisci_parola()) # send con tasto invio
+        self.txt.bind("<Return>", lambda event: self.inserisci_parola())  # send con tasto invio
 
-        self.lbl = Label(self.window, text="inserisci termini", font=font)
-        self.lbl.grid(column=0, row=4)
-
-        self.btn = Button(self.window, text="invia", command=self.inserisci_parola, font=font)   #salva button
+        self.btn = Button(self.window, text="Send", command=self.inserisci_parola, font=font)   #salva button
         self.btn.grid(column=0, row=5)
 
-        self.lbl = Label(self.window, text="Termini inseriti: ", font=font)
-        self.lbl.grid(column=0, row=6)
 
-        self.lbl = Label(self.window, text="--Seleziona modalità di esecuzione:", font=font)
+        self.lbl = Label(self.window, text="--Select execution mode:", font=font)
         self.lbl.grid(column=1, row=0)
 
         self.var = IntVar()
+        self.testo2 = ""
 
-        def sel():
-            selection = "Mode: " + str(self.var.get())
-            self.lbl2.config(text=selection)
-            self.compute()
+        self.R1 = Radiobutton(self.window, text="1) Historical", variable=self.var, value=1, command=self.compute)
 
-        self.R1 = Radiobutton(self.window, text="1) Historical", variable=self.var, value=1, command=sel)
+        self.R1.grid(column=1, row=2)
 
-        self.R1.grid(column=1,row=2)
+        self.R2 = Radiobutton(self.window, text="2) Real time", variable=self.var, value=2, command=self.compute)
+        self.R2.grid(column=1, row=3)
 
-        self.R2 = Radiobutton(self.window, text="2) Real time", variable=self.var, value=2, command=sel)
-        self.R2.grid(column=1,row=3)
+        self.lbl = Label(self.window, text="", font=font)
+        self.lbl.grid(column=0, row=7)
 
         self.lbl2 = Label(self.window, text="Select mode")
         self.lbl2.grid(column=1, row=4)
 
-        self.window.mainloop()
+        self.lbl3 = Label(self.window, text="Enter at least one word")
+        self.lbl3.grid(column=0, row=10)
 
-    def inserisci_parola(self):
+        self.btn_search = Button(self.window, text="Compute", command=self.get_command)
 
-        self.testo=self.txt.get()
-        print(f"testo scritto: '{self.testo}'")
-
-        self.testo2=self.testo
-        self.testo2=self.testo2.split(' ')
-        self.testo2=list(filter(None,self.testo2)) #per ottenere una lista senza spazi
-        self.stringa= ' '.join(self.testo2)
-
-        self.lbl = Label(self.window, text=f"\t\t\t\t\t", font=font)
-        self.lbl.grid(column=0, row=7)
-
-        self.lbl = Label(self.window, text=f"'{self.stringa}'", font=font)
-        self.lbl.grid(column=0, row=7)
-
-        self.lbl = Label(self.window, text=f"'numero parole: {len(self.testo2)}'", font=font)
-        self.lbl.grid(column=0, row=8)
-        self.btn_search = Button(self.window, text="Compute")
         self.btn_search.grid(column=1, row=10)
         self.btn_search.config(state="disabled")
+
+    def inserisci_parola(self):
+        self.testo = self.txt.get()
+        print("testo scritto: {}".format(self.testo))
+
+        self.testo2 = self.testo
+        self.testo2 = self.testo2.split(' ')
+        self.testo2 = list(filter(None, self.testo2))  # per ottenere una lista senza spazi
+        self.stringa = ','.join(self.testo2)
+
+        self.lbl.config(text="".format(self.stringa), font=font)
+
         self.compute()
 
     def compute(self):
-
-        if str(self.var.get()) == '1': # Historical mode (almeno 1 parola)
-
-            if len(self.testo2)<1:
+        if str(self.var.get()) == '1':  # Historical mode (almeno 1 parola)
+            self.lbl2.config(text="Mode: Historical Tweets")
+            if len(self.testo2) < 1:
                 self.btn_search.config(state="disabled")
-                self.lbl3 = Label(self.window, text="\tInserisci almeno 1 parola\t")
-                self.lbl3.grid(column=0, row=10)
+                self.lbl3.config(text="Enter at least one word")
             else:
                 self.btn_search.config(state="active")
-                self.lbl3 = Label(self.window, text="\t\t\t\t")
-                self.lbl3.grid(column=0, row=10)
-                self.btn_search = Button(self.window, text="Compute")
-                self.btn_search.grid(column=1, row=10)
+                self.lbl3.config(text="")
 
         elif str(self.var.get()) == '2':  # Real time mode (almeno 3 parole)
-
+            self.lbl2.config(text="Mode: RT")
             if len(self.testo2) < 3:
                 self.btn_search.config(state="disabled")
-                self.lbl3 = Label(self.window, text="\tInserisci almeno 3 parole\t")
-                self.lbl3.grid(column=0, row=10)
-                #self.btn_search.grid_forget()
+                self.lbl3.config(text="Enter at least three words")
+
             else:   #bottone
                 self.btn_search.config(state="active")
-                self.lbl3 = Label(self.window, text="\t\t\t\t")
-                self.lbl3.grid(column=0, row=10)
-                self.btn_search = Button(self.window, text="Compute")
-                self.btn_search.grid(column=1, row=10)
+                self.lbl3.config(text="")
 
-        elif str(self.var.get()) != '1' and str(self.var.get()) != '2':   #no mode selected
+        elif str(self.var.get()) != '1' and str(self.var.get()) != '2':   # no mode selected
+            self.lbl3.config(text="Select a search mode")
 
-            self.lbl3 = Label(self.window, text="Seleziona una modalità di ricerca")
-            self.lbl3.grid(column=0, row=10)
+    def call_client(self):
+        path = os.path.join(os.path.dirname(__file__), "TWclient.py")
+        os.system("xterm -hold -e python3  {} {} {}".format(path, self.stringa, self.var.get()))
+
+    def get_command(self):
+        threading.Thread(target=self.call_client).start()
 
 
-g=GUI()
-g.main()
+if __name__ == "__main__":
+    root = Tk()
+    foo = GUI(root)
+    root.mainloop()
